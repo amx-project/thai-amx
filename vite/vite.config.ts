d3c3d1e50e757fca6b74722d0739cd8048134500
Hidenori <hfu@users.noreply.github.com>
2025-04-19 17:45:34 +0900

    update

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   server: {
     host: true
+  },
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
 
